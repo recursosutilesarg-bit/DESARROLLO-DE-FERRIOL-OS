3b3c518344067962650914676704bf1f79a4c2c6
recursosutilesarg-bit <recursosutilesarg@gmail.com>
2026-04-21 13:02:41 -0300

    Add files via upload

--- kiosco-config.js
+++ kiosco-config.js
@@ -7,6 +7,6 @@
     SUPABASE_URL: 'https://ctvdrqxnrrjfwifixecf.supabase.co',
     SUPABASE_ANON_KEY:
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0dmRycXhucnJqZndpZml4ZWNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzNzY5NjcsImV4cCI6MjA4Njk1Mjk2N30.U2Y5IBY7fSFjmgiGkyYX8BLdRyU1ZNFqVEEIaT7QIJI',
-    APP_URL: 'https://recursosutilesarg-bit.github.io/GRUPO-FERRIOL/kiosco.html',
+    APP_URL: 'https://recursosutilesarg-bit.github.io/DESARROLLO-DE-FERRIOL-OS/kiosco.html',
   };
 })();
